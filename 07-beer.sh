e2535dc9b6fb6dfea5249241309472a17708b259
dmusau23 <danmusau09@gmail.com>
2026-01-31 22:37:18 -0500

    Completed exercise 07-beer.sh

--- 07-beer.sh
+++ 07-beer.sh
@@ -23,6 +23,30 @@ done
 # exercise: implement another counting song (such as 12 days of Christmas) 
 # using loops and if statements.
 
+echo "Now let's sing the 12 Days of Christmas"
+
+day=1
+
+while [ $day -le 12 ]; do
+  echo "On the $day day of Christmas, my true love gave to me"
+
+  if [ $day -ge 1 ]; then echo "a partridge in a pear tree"; fi
+  if [ $day -ge 2 ]; then echo "two turtle doves"; fi
+  if [ $day -ge 3 ]; then echo "three french hens"; fi
+  if [ $day -ge 4 ]; then echo "four calling birds"; fi
+  if [ $day -ge 5 ]; then echo "five golden rings"; fi
+  if [ $day -ge 6 ]; then echo "six geese a-laying"; fi
+  if [ $day -ge 7 ]; then echo "seven swans a-swimming"; fi
+  if [ $day -ge 8 ]; then echo "eight maids a-milking"; fi
+  if [ $day -ge 9 ]; then echo "nine ladies dancing"; fi
+  if [ $day -ge 10 ]; then echo "ten lords a-leaping"; fi
+  if [ $day -ge 11 ]; then echo "eleven pipers piping"; fi
+  if [ $day -ge 12 ]; then echo "twelve drummers drumming"; fi
+
+  echo ""
+  ((day = day + 1))
+done
+
 echo "Now we sing about monkeys jumping on the bed"
 echo "How many monkeys?"
 read count
